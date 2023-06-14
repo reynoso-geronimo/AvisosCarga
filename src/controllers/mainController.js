8b5de9095ea0e0df238383c9bf21865fff03ebd9
Reynoso <reynoso.geronimo@gmail.com>
2023-06-14 15:54:46 -0300

    .

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,10 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 let permisos = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../permisos.json"),"utf-8"))
-
+let queue = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../queue.json"),"utf-8"))
 module.exports = {
     index:(req,res)=>{
-        console.log(permisos)
-        res.render("index",{permisos:permisos})
+        
+        res.render("index",{permisos:permisos, queue:queue})
+    },
+    nuevo:(req,res)=>{
+        res.render("nuevo")
+    },
+    agregar:(req,res)=>{
+     
+      
+        queue= [...queue,req.body]
+        fs.writeFileSync(path.resolve(__dirname, "../../queue.json"),JSON.stringify(queue),null,2)
+        res.redirect("/")
     }
 }
--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -4,4 +4,7 @@ const mainController = require('../controllers/mainController.js');
 
 router.get("/", mainController.index);
 
+router.get("/nuevo",mainController.nuevo)
+router.post("/nuevo",mainController.agregar)
+
 module.exports = router
