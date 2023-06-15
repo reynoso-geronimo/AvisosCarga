89e02ef502d36d5d608040b79ecfa43d5c24f859
Reynoso <reynoso.geronimo@gmail.com>
2023-06-15 16:52:43 -0300

    .

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,21 +1,78 @@
 const fs = require('fs');
 const path = require('path');
 
-let permisos = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../permisos.json"),"utf-8"))
-let queue = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../queue.json"),"utf-8"))
+const leerPermisos = () => JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../permisos.json"), "utf-8"))
+
+const escribirPermisos = (permisos) => fs.writeFileSync(path.resolve(__dirname, "../../permisos.json"), JSON.stringify(permisos, null, 2))
+
+
+
+
+
 module.exports = {
-    index:(req,res)=>{
+    index: (req, res) => {
+        const permisos = leerPermisos().filter(permiso=>permiso.estado!="BAJA")
+        permisos.forEach(permiso => {
+            permiso.proximoAviso= new Date(permiso.proximoAviso).toLocaleString("en-gb")
+            
+        });
+        return res.render("index", { permisos: permisos })
+    },
+    baja: (req, res) => {
+        const permisos = leerPermisos().filter(permiso=>permiso.estado=="BAJA")
+        return res.render("baja", { permisos: permisos })
+    },
+    nuevo: (req, res) => {
+        return res.render("nuevo")
+    },
+    agregar: (req, res) => {
+        let nuevoPe = { ...req.body,proximoAviso:new Date(req.body.proximoAviso), ultimoAviso: null, estado: null }
+        let permisos = [...leerPermisos(), nuevoPe]
+
+        escribirPermisos(permisos)
+        return res.redirect("/")
+    },
+    editarForm: (req, res) => {
+        return res.render("editar", { permiso: leerPermisos().find(permiso => permiso.permiso == req.params.permiso) })
+    },
+    editar: (req, res) => {
+        const permisos = leerPermisos()
+        const permisoEliminar = permisos.find(permiso => permiso.permiso == req.params.permiso);
+        permisoEliminar.permiso = req.body.permiso
+        permisoEliminar.aduana = req.body.aduana
+        permisoEliminar.localidad = req.body.localidad
+        permisoEliminar.direccion = req.body.direccion
+        permisoEliminar.proximoAviso = req.body.proximoAviso
         
-        res.render("index",{permisos:permisos, queue:queue})
+        escribirPermisos(permisos)
+
+        return res.redirect("/")
     },
-    nuevo:(req,res)=>{
-        res.render("nuevo")
+
+
+    eliminar: (req, res) => {
+        const permisos = leerPermisos()
+        const permisoEliminar = permisos.find(permiso => permiso.permiso == req.params.permiso);
+        permisoEliminar.estado = "BAJA"
+        
+        escribirPermisos(permisos)
+
+        return res.redirect("/")
     },
-    agregar:(req,res)=>{
-     
-        let nuevoPe ={...req.body,  ultimoAviso:null , estado:null}
-        queue= [...queue,nuevoPe]
-        fs.writeFileSync(path.resolve(__dirname, "../../queue.json"),JSON.stringify(queue,null,2))
-        res.redirect("/")
-    }
+    
+    activar: (req, res) => {
+        const permisos = leerPermisos()
+        const permisoEliminar = permisos.find(permiso => permiso.permiso == req.params.permiso);
+        permisoEliminar.estado = null
+        
+        escribirPermisos(permisos)
+
+        return  res.redirect("/")
+    },
+
+    permisos: (req, res) => {
+        const permisos = leerPermisos().filter(permiso=>permiso.estado!="BAJA")
+         return res.send(permisos)
+    },
+
 }
--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -3,8 +3,16 @@ const router = express.Router()
 const mainController = require('../controllers/mainController.js');
 
 router.get("/", mainController.index);
+router.get("/baja", mainController.baja);
 
 router.get("/nuevo",mainController.nuevo)
 router.post("/nuevo",mainController.agregar)
+router.get("/editar/:permiso", mainController.editarForm);
+router.post("/editar/:permiso", mainController.editar);
+router.post("/eliminar/:permiso", mainController.eliminar);
+router.post("/activar/:permiso", mainController.activar);
+
+
+router.get("/permisos", mainController.permisos);
 
 module.exports = router
