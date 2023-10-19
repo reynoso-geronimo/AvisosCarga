97b9a6eb0c311a7e527f2fa761e192446be66173
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-10-19 08:02:09 -0300

    estilos y validaciones de front

---
+++ public/js/new.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const permiso = document.getElementById("permiso");
+    const dateTime = document.getElementById("proximoAviso");
+    const form = document.getElementById("form")
+    const regex = /^(\d{2})(\d{3})([A-Za-z]{2}\d{2})(\d{6}[A-Za-z])$/;
+
+
+
+    form.addEventListener('submit', (e) => {
+        const selectedDateTime = new Date(dateTime.value);
+        const dateNow = new Date();
+        let errors= false
+        e.preventDefault()
+        if (!regex.test(permiso.value)) {
+            permiso.classList.add('input-error')
+            errors=true
+        } if (selectedDateTime < dateNow) {
+            dateTime.classList.add('input-error')
+            errors=true
+        } 
+
+           !errors&&form.submit()
+
+        
+
+    })
+
+})
