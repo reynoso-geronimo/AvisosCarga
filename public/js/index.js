70231605e4d791749a9b8ab308595fc97494d1bf
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-10-20 08:01:18 -0300

    agregando spinner al index

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,17 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  
 
-
-    const eventSource = new EventSource("/events");
-    eventSource.addEventListener("open", (e) => {
-        console.log("The connection has been established.");
-      });
-    eventSource.addEventListener("message", (event) => {
-    // Maneja el evento y actualiza la página según sea necesario
-    const data = JSON.parse(event.data);
-    console.log("Se ha actualizado la información:", data);
-    location.reload()
+  const eventSource = new EventSource("/events");
+  eventSource.addEventListener("open", (e) => {
+      console.log("The connection has been established.");
+    });
+  eventSource.addEventListener("message", (event) => {
+  // Maneja el evento y actualiza la página según sea necesario
+  const data = JSON.parse(event.data);
+  console.log("Se ha actualizado la información:", data);
+  location.reload()
 });
+
+const botones = document.querySelectorAll('.btn-action')
+console.log(botones)
+})
