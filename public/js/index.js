b557433922f41ebaf8412e83b2d865fd476c6368
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-10-21 18:47:17 -0300

    notificaciones de escritorio cuando ocurre un error

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,45 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  
+document.addEventListener('DOMContentLoaded', () => {
+  // Solicitar permiso de notificación cuando se carga la página
+  Notification.requestPermission();
 
   const eventSource = new EventSource("/events");
   eventSource.addEventListener("open", (e) => {
-      console.log("The connection has been established.");
-    });
+    console.log("La conexión se ha establecido.");
+  });
   eventSource.addEventListener("message", (event) => {
-  // Maneja el evento y actualiza la página según sea necesario
-  const data = JSON.parse(event.data);
-  console.log("Se ha actualizado la información:", data);
-  location.reload()
+  
+    const data = JSON.parse(event.data);
+    console.log("Se ha actualizado la información:", data.data.aviso);
+
+    if (Notification.permission === "granted" && data.data.aviso.estado != undefined && data.data.aviso.estado == "error") {
+      new Notification("Nuevo mensaje", {
+        body: "Ocurrió un error al generar el aviso",
+      });
+    }
+
+    location.reload();
+  });
+
+  const btnEdit = document.querySelectorAll('.btn-edit-a');
+  btnEdit.forEach(btn => {
+    btn.addEventListener("click", () => {
+      document.getElementById("spinner").classList.add("show");
+    });
+  });
+
+  const btnDelete = document.querySelectorAll('#delete-file');
+  btnDelete.forEach(btn => {
+    btn.addEventListener("submit", () => {
+      document.getElementById("spinner").classList.add("show");
+    });
+  });
 });
 
-const btnEdit = document.querySelectorAll('.btn-edit-a')
-  btnEdit.forEach(btn => {
-    btn.addEventListener("click",()=>{
-      document.getElementById("spinner").classList.add("show");
-    })
-  });
-   
-    const btnDelete = document.querySelectorAll('#delete-file')
-    btnDelete.forEach(btn => {
-      btn.addEventListener("submit",()=>{
-        document.getElementById("spinner").classList.add("show");
-      })
-    });
-    
 
-})
 window.addEventListener("unload", () => {
-  btnEdit.removeEventListener("click", showSpinner);
+  btnEdit.forEach(btn => {
+    btn.removeEventListener("click", () => {
+      document.getElementById("spinner").classList.add("show");
+    });
+  });
 });
